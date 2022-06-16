d77aa0b9dcbb65209c7fbc678bd281a8c52fa82b
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-06-16 10:47:52 +0700

    uyen.com

--- module3/_case_study_database/Task4.sql
+++ module3/_case_study_database/Task4.sql
@@ -1,3 +1,10 @@
 USE furama;
 
-SELECT * from nhan_vien WHERE ( ho_ten LIKE "H%" )OR (ho_ten LIKE "T%") OR( ho_ten LIKE "K%") ;
+-- Task 4: Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần.
+--  Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+SELECT  kh.ma_khach_hang, kh.ho_ten ,count(hd.ma_hop_dong) AS "số lần đặt phòng", lk.ten_loai_khach FROM hop_dong hd 
+JOIN khach_hang kh ON hd.ma_khach_hang=kh.ma_khach_hang
+JOIN loai_khach lk ON kh.ma_loai_khach=lk.ma_loai_khach
+WHERE lk.ten_loai_khach = "Diamond"
+GROUP BY(ma_khach_hang) ORDER BY "số lần đặt phòng";
