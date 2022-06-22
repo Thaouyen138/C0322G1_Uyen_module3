7ab0156c76ca65d34d17df8c532907ac98e5dec4
Thaouyen138 <96711178+Thaouyen138@users.noreply.github.com>
2022-06-22 21:36:32 +0700

    uyendepgai.com

--- module3/_case_study_database/Task19.sql
+++ module3/_case_study_database/Task19.sql
@@ -1,5 +1,7 @@
 Use furama;
 -- 19.	Cập nhật giá cho các dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2020 lên gấp đôi.
 UPDATE dich_vu_di_kem SET gia = gia * 2
-WHERE ma_dich_vu_di_kem IN
-(SELECT ma_dich_vu_di_kem  FROM hop_dong_chi_tiet GROUP BY ma_dich_vu_di_kem HAVING count(ma_dich_vu_di_kem)  >= 3 );
+WHERE  ma_dich_vu_di_kem IN
+(SELECT ma_dich_vu_di_kem  FROM hop_dong_chi_tiet hdct
+ join hop_dong hd ON hd.ma_hop_dong=hdct.ma_hop_dong where year(hd.ngay_lam_hop_dong) =2020
+ GROUP BY ma_dich_vu_di_kem HAVING count(ma_dich_vu_di_kem)  >= 3 );
